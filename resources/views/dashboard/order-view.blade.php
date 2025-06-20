d08625e53913687f271e6eb27146dda1699003e6
PINO Gabriele <pino28ga@lycee-ndduroc.com>
2025-06-20 11:34:53 +0200

    filter

--- resources/views/dashboard/order-view.blade.php
+++ resources/views/dashboard/order-view.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
             <div class="row">
 
-                <div class="d-flex gap-4 mb-4">
+                <div class="d-flex gap-4 mb-2">
 
                     <a href="{{ route('view',['state'=>'en_attente']) }}"
                        class="btn btn-{{ session()->get('orderStateExpected') == 'en_attente' ? 'primary' : 'outline-primary' }} mr-2">
@@ -17,6 +17,24 @@
                        class="btn btn-{{ session()->get('orderStateExpected') == 'terminer' ? 'success' : 'outline-success' }} mr-2">
                         Terminées </a>
 
+                </div>
+
+                <button class="btn btn-secondary mb-2 mr-2" type="button" data-toggle="collapse" data-target="#searchForm" aria-expanded="false" aria-controls="searchForm">
+                    <i class="fa fa-filter"></i>
+                </button>
+
+                <div class="collapse" id="searchForm">
+                    <form method="GET" action="{{ route('view', ['state' => session()->get('orderStateExpected')]) }}">
+                        <div class="row g-3 align-items-center mb-2">
+                            <div class="col-auto">
+                                <input type="date" id="date" name="date" class="form-control"
+                                       value="{{ request('date') }}">
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-secondary">Rechercher</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
 
             </div>
